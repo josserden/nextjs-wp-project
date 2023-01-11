f36c48474fb38c549ff62aada8913f1e95e91fac
Den <52295780+josserden@users.noreply.github.com>
2023-01-11 18:28:41 +0200

    create first query

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,36 @@
-export default function Home() {
+import client from 'client';
+import { gql } from '@apollo/client';
+
+const Home = ({ data }) => {
+  console.log(data);
+
   return <div>Next JS &amp; WordPress course.</div>;
-}
+};
+
+export default Home;
+
+export const getStaticProps = async () => {
+  const {
+    data: {
+      nodeByUri: { blocksJSON },
+    },
+  } = await client.query({
+    query: gql`
+      query NewQuery {
+        nodeByUri(uri: "/") {
+          ... on Page {
+            id
+            title
+            blocksJSON
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      data: JSON.parse(blocksJSON),
+    },
+  };
+};
