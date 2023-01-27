8c3e978dab1aa5a5c22d42ce31270417732632da
Den <52295780+josserden@users.noreply.github.com>
2023-01-27 15:24:45 +0200

    update class

--- components/Columns/Columns.jsx
+++ components/Columns/Columns.jsx
@@ -4,9 +4,7 @@ export const Columns = ({ isStackedOnMobile, children }) => {
   return (
     <section className="py-10">
       <div
-        className={`${
-          isStackedOnMobile ? 'block md:flex' : 'flex'
-        } mx-auto max-w-5xl`}
+        className={`container ${isStackedOnMobile ? 'block md:flex' : 'flex'}`}
       >
         {children}
       </div>
