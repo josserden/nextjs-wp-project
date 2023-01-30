d0cdcf9b837bc323530032c10fb3d79639c66c3a
Den <52295780+josserden@users.noreply.github.com>
2023-01-30 14:42:13 +0200

    fix mistake

--- components/index.js
+++ components/index.js
@@ -6,7 +6,7 @@ export * from './Columns/Columns';
 export * from './Cover/Cover';
 export * from './Filters/Filters';
 export * from './FormInput/FormInput';
-export * from './FormInput/FormLabel';
+export * from './FormInput/FormInput';
 export * from './Heading/Heading';
 export * from './MainMenu/MainMenu';
 export * from './Page/Page';
