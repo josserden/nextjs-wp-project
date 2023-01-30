3f6765918514a5ae8ed4f21a23d4ffb871bdc771
Den <52295780+josserden@users.noreply.github.com>
2023-01-30 21:18:50 +0200

    clean code

--- components/FormspreeForm/FormspreeForm.jsx
+++ components/FormspreeForm/FormspreeForm.jsx
@@ -7,50 +7,58 @@ export const FormspreeForm = ({ formId }) => {
 
   if (state.succeeded) {
     return (
-      <div className="container py-20">
-        <b className=" block text-center font-body text-3xl uppercase text-sky-700">
-          Thanks for joining!
-        </b>
-      </div>
+      <section className="py-20">
+        <div className="container">
+          <b className=" block text-center font-body text-3xl uppercase text-sky-700">
+            Thanks for joining!
+          </b>
+        </div>
+      </section>
     );
   }
 
   return (
-    <div className="container ">
-      <form
-        onSubmit={handleSubmit}
-        className="mx-auto grid max-w-3xl grid-cols-1 gap-4 rounded-md border bg-slate-50 px-7 py-10 shadow-md"
-      >
-        <div className="grid grid-cols-1 gap-2">
-          <label htmlFor="email">
-            <span className="text-gray-700">Email Address</span>
-          </label>
-          <FormInput id="email" type="email" name="email" />
-          <ValidationError prefix="Email" field="email" errors={state.errors} />
-        </div>
+    <section className="py-20">
+      <div className="container">
+        <form
+          onSubmit={handleSubmit}
+          className="mx-auto grid max-w-3xl grid-cols-1 gap-4 rounded-md border bg-slate-50 px-7 py-10 shadow-md"
+        >
+          <div className="grid grid-cols-1 gap-2">
+            <label htmlFor="email">
+              <span className="text-gray-700">Email Address</span>
+            </label>
+            <FormInput id="email" type="email" name="email" />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+          </div>
 
-        <div className="grid grid-cols-1 gap-2">
-          <label htmlFor="message">Message</label>
-          <FormInput
-            className="resize-none"
-            $as={'textarea'}
-            id="message"
-            name="message"
-          />
-          <ValidationError
-            prefix="Message"
-            field="message"
-            errors={state.errors}
-          />
-        </div>
+          <div className="grid grid-cols-1 gap-2">
+            <label htmlFor="message">Message</label>
+            <FormInput
+              className="resize-none"
+              $as={'textarea'}
+              id="message"
+              name="message"
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+          </div>
 
-        <div className="relative">
-          <button type="submit" className="btn" disabled={state.submitting}>
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
+          <div className="relative">
+            <button type="submit" className="btn" disabled={state.submitting}>
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 };
 
